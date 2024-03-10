58ea43de69f466ff98311193f9d3a3e950e38f7b
MARCOS ROCHA <marcos.rocha@ths.inf.br>
2024-03-10 12:58:57 -0300

    Add workerservice

---
+++ Subscriptions.WorkerService/Program.cs
@@ -0,0 +1,10 @@
+using Subscriptions.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
