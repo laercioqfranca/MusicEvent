2c2652205e6be09505c3fc427a33b2d6b11f4d62
MARCOS ROCHA <marcos.rocha@ths.inf.br>
2024-03-14 12:08:25 -0300

    Update migration MusicEvent

--- MusicEvent.Infra.Data/Migrations/20240314150650_updateInscricao.Designer.cs
+++ MusicEvent.Infra.Data/Migrations/20240314150650_updateInscricao.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicEvent.Infra.Data.Context;
 
@@ -11,9 +12,10 @@ using MusicEvent.Infra.Data.Context;
 namespace MusicEvent.Infra.Data.Migrations
 {
     [DbContext(typeof(MusicEventContext))]
-    partial class MusicEventContextModelSnapshot : ModelSnapshot
+    [Migration("20240314150650_updateInscricao")]
+    partial class updateInscricao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MusicEvent.Infra.Data/Migrations/20240314150650_updateInscricao.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicEvent.Infra.Data.Migrations
+{
+    public partial class updateInscricao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscricao");
+
+            migrationBuilder.DropTable(
+                name: "LogHistorico");
+
+            migrationBuilder.CreateTable(
+                name: "Subscription",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdEvento = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscription", x => new { x.IdUsuario, x.IdEvento });
+                    table.ForeignKey(
+                        name: "FK_Subscription_Evento_IdEvento",
+                        column: x => x.IdEvento,
+                        principalTable: "Evento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Subscription_Usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscription_IdEvento",
+                table: "Subscription",
+                column: "IdEvento");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscription");
+
+            migrationBuilder.CreateTable(
+                name: "Inscricao",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdEvento = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscricao", x => new { x.IdUsuario, x.IdEvento });
+                    table.ForeignKey(
+                        name: "FK_Inscricao_Evento_IdEvento",
+                        column: x => x.IdEvento,
+                        principalTable: "Evento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Inscricao_Usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LogHistorico",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EntidadeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NomeEntidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    TipoLog = table.Column<short>(type: "smallint", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogHistorico", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricao_IdEvento",
+                table: "Inscricao",
+                column: "IdEvento");
+        }
+    }
+}
