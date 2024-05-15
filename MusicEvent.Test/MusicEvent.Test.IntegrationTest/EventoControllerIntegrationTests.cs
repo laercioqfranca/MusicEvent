a61d1c5030d93d2ea0ded411f8edb5ad51b5b57a
LAERCIO DE QUEIROS FRANCA JUNIOR <laercio.junior@ths.inf.br>
2024-05-15 09:53:35 -0300

    Implementação dos testes de unidade e integração.

---
+++ MusicEvent.Test/MusicEvent.Test.IntegrationTest/EventoControllerIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+namespace MusicEvent.Test.IntegrationTest
+{
+    public class EventoControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public EventoControllerIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOkResult_WhenCalled()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/v1/Evento/GetAll");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ MusicEvent.Test/MusicEvent.Test.UnitTest/EventoControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using MusicEvent.Web.Controllers;
+using MusicEvent.Application.Interfaces;
+using MusicEvent.Core.Interfaces;
+using MusicEvent.Core.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using MusicEvent.Application.ViewModels;
+
+namespace MusicEvent.Test.UnitTest
+{
+    public class EventoControllerTests
+    {
+        private readonly Mock<IEventoAppService> _mockAppService;
+        private readonly Mock<IMediatorHandler> _mockMediatorHandler;
+        private readonly EventoController _controller;
+        private readonly Mock<DomainNotificationHandler> _mockNotificationHandler;
+
+
+        public EventoControllerTests()
+        {
+            _mockAppService = new Mock<IEventoAppService>();
+            _mockNotificationHandler = new Mock<DomainNotificationHandler>();
+            _mockMediatorHandler = new Mock<IMediatorHandler>();
+            _controller = new EventoController(_mockAppService.Object, _mockNotificationHandler.Object, _mockMediatorHandler.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOkResult_WhenCalled()
+        {
+            // Arrange
+            var expectedList = new List<EventoViewModel> { new EventoViewModel(), new EventoViewModel() };
+            _mockAppService.Setup(service => service.GetAll()).ReturnsAsync(expectedList);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = okResult.Value;
+            var data = returnValue?.GetType().GetProperty("data")?.GetValue(returnValue);
+            var success = returnValue?.GetType().GetProperty("success")?.GetValue(returnValue);
+            Assert.NotNull(data);
+            Assert.NotNull(success);
+            Assert.True((bool)success);
+            Assert.Equal(expectedList, data as List<EventoViewModel>);
+            _mockAppService.Verify(service => service.GetAll(), Times.Once);
+        }
+    }
+}
